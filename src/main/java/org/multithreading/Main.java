91e068e2a0c030af4709f122751e1198e86ad7d8
Pomog <thoryur@gmail.com>
2023-10-25 22:53:15 +0300

    refactor

--- src/main/java/org/multithreading/Main.java
+++ src/main/java/org/multithreading/Main.java
@@ -2,32 +2,24 @@ package org.multithreading;
 
 public class Main {
 
-    private static String threadCode;
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws InterruptedException {
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                long startTime = System.currentTimeMillis();
-                for (int i = 0; i < 10; i++) {
-                    Brackets.generate("Thread 1");
-                }
-                long endTime = System.currentTimeMillis();
-                System.out.println("Time taken for Thread 1: " + (endTime - startTime) + "ms");
+        new Thread(() -> {
+            long startTime = System.currentTimeMillis();
+            for (int i = 0; i < 10; i++) {
+                Brackets.generate("Thread 1");
             }
+            long endTime = System.currentTimeMillis();
+            System.out.println("Time taken for Thread 1: " + (endTime - startTime) + "ms");
         }).start();
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                long startTime = System.currentTimeMillis();
-                for (int i = 0; i < 10; i++) {
-                    Brackets.generate("Thread 2");
-                }
-                long endTime = System.currentTimeMillis();
-                System.out.println("Time taken for Thread 2:  " + (endTime - startTime) + "ms");
+        new Thread(() -> {
+            long startTime = System.currentTimeMillis();
+            for (int i = 0; i < 10; i++) {
+                Brackets.generate("Thread 2");
             }
+            long endTime = System.currentTimeMillis();
+            System.out.println("Time taken for Thread 2:  " + (endTime - startTime) + "ms");
         }).start();
     }
 
