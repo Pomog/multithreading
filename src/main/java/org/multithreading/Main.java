a1d279edb4223ca3f8ec8cd6e0ed9544ee17be40
Pomog <thoryur@gmail.com>
2023-10-26 18:52:13 +0300

    volatile

--- src/main/java/org/multithreading/Main.java
+++ src/main/java/org/multithreading/Main.java
@@ -2,7 +2,29 @@ package org.multithreading;
 
 public class Main {
 
+    volatile public static int flag = 0;
+
     public static void main(String[] args) {
+
+        new Thread (() -> {
+            while (true){
+                if (Main.flag == 0) {
+                    System.out.println("Runing");
+                } else {
+                    System.out.println("Not runing");
+                    break;
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(5000);
+                Main.flag = 1;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
 
         new Thread(() -> {
             long startTime = System.currentTimeMillis();
